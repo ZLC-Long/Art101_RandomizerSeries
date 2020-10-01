b647a66311c77a46014c4db77a24eb1f9e233ae4
LeungZa <60365839+LeungZa@users.noreply.github.com>
2020-10-01 12:03:27 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -13,9 +13,20 @@ let games = [{
 ];
 
 let randomIndex;
+// let counter = 0;
+let animating = false;
 
 function setup() {
   createCanvas(400, 400);
+  background(200);
+  textSize(32);
+
+  text("click to randomize", 50, 50);
+
+  // This is play changing BACKGROUND COLOR
+  // setTimeout(changeBackground, 1000);
+  // setInterval(changeBackground, 1000);
+
 
   /*console.log(games[3]);*/
   /*console.log(games.length); */
@@ -41,15 +52,65 @@ function setup() {
 
 }
 
-function draw() {}
-function mousePressed(){
-  background(random(200, 220));
-  //Pull Random Item
-  // console.log(random(5));
-  randomIndex = int(random(games.length));
-  // console.log(games[randomIndex].name);
-  text(games[randomIndex].name, 50, 50 /*number means position on canvas*/);
-  //delete it out of random index
-  games.splice(randomIndex, 1);
-  // console.log(games);
+function draw() {
+  if (animating == true) {
+    ellipse(random(width), random(height), random(50, 200));
+  }
+
+
+}
+
+// This is play to TIMEOUT changing BACKGROUND function
+// function changeBackground() {
+//   counter++;
+//   console.log(counter);
+//   if (counter <= 5) {
+//     background(random(255), random(255), random(255));
+//     setTimeout(changeBackground, 1000);
+//   } else {
+//   }
+// }
+
+function randomizer() {
+  animating = false;
+
+  if (games[0]) {
+    background(random(200, 220));
+    //Pull Random Item
+    // console.log(random(5));
+    randomIndex = int(random(games.length));
+    // console.log(games[randomIndex].name);
+    text(`${games[randomIndex].name}'s mechanic is
+     ${games[randomIndex].mechanic}`, 50, 50 /*number means position on canvas*/ );
+    // text(games[randomIndex].name + "'s mechanic is "
+    //+ games[randomIndex].mechanic, 50, 50 /*number means position on canvas*/ );
+    //And other code: text(`${games.name} hahaha`, 50, 50);
+    //delete it out of random index
+    games.splice(randomIndex, 1);
+    // console.log(games);
+  } else {
+    background(random(200, 220));
+    text("nothing left!!", 50, 50)
+  }
+}
+
+function mousePressed() {
+  animating = true;
+  setTimeout(randomizer, 2000);
+
+  // if (games[0]) {
+  //   background(random(200, 220));
+  //   //Pull Random Item
+  //   // console.log(random(5));
+  //   randomIndex = int(random(games.length));
+  //   // console.log(games[randomIndex].name);
+  //   text(games[randomIndex].name, 50, 50 /*number means position on canvas*/ );
+  //   //text(`${games.name} hahaha`, 50, 50);
+  //   //delete it out of random index
+  //   games.splice(randomIndex, 1);
+  //   // console.log(games);
+  // } else {
+  //   background(random(200, 220));
+  //   text("nothing left!!", 50, 50)
+  // }
 }
